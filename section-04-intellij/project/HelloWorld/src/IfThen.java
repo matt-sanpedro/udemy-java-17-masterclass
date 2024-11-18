d578a747d0eddf2f2bc819aa59aecbb164b0cf9f
matt-sanpedro <mattsanpedro7@gmail.com>
2024-11-18 10:17:17 -0800

    Added example on condition for if statement

--- section-04-intellij/project/HelloWorld/src/IfThen.java
+++ section-04-intellij/project/HelloWorld/src/IfThen.java
@@ -21,5 +21,14 @@ public class IfThen {
             System.out.println("Either or both of the conditions are true");
         }
 
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
     }
 }

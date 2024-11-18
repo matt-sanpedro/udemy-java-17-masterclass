fabf3d8e6076dda25855a06f05f3ffd732d405c0
matt-sanpedro <mattsanpedro7@gmail.com>
2024-11-17 18:20:38 -0800

    Added examples using logical and operator (&&)

--- section-04-intellij/project/HelloWorld/src/IfThen.java
+++ section-04-intellij/project/HelloWorld/src/IfThen.java
@@ -6,6 +6,17 @@ public class IfThen {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of ET");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
     }
 
 }
